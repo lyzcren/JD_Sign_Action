ec122c6679d68ab1f3945e2f964696e6a7dcd6c4
lyzcren <lyzcren@hotmail.com>
2021-02-19 17:33:44 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -13,6 +13,8 @@ const cookie = process.env.JD_COOKIE
 const dual_cookie = process.env.JD_DUAL_COOKIE
 // Server酱SCKEY
 const push_key = process.env.PUSH_KEY
+// Server酱SCKEY（Turbo 版）
+const push_key_turbo = process.env.PUSH_KEY_TURBO
 
 // 京东脚本文件
 const js_url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
@@ -63,7 +65,45 @@ function setupCookie() {
 
 function sendNotificationIfNeed() {
 
-  if (!push_key) {
+  if (!push_key && !push_key_turbo) {
+    console.log('执行任务结束!'); return;
+  }
+
+  if (!fs.existsSync(result_path)) {
+    console.log('没有执行结果，任务中断!'); return;
+  }
+
+  let text = "京东签到_" + dateFormat();
+  let desp = fs.readFileSync(result_path, "utf8")
+
+  // 去除末尾的换行
+  let SCKEY = push_key.replace(/[\r\n]/g,"")
+
+  const options ={
+    uri:  `https://scapi.ftqq.com/${SCKEY}.send`,
+    form: { text, desp },
+    json: true,
+    method: 'POST'
+  }
+
+  rp.post(options).then(res=>{
+    const code = res['errno'];
+    if (code == 0) {
+      console.log("通知发送成功，任务结束！")
+    }
+    else {
+      console.log(res);
+      console.log("通知发送失败，任务中断！")
+      fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
+    }
+  }).catch((err)=>{
+    console.log("通知发送失败，任务中断！")
+    fs.writeFileSync(error_path, err, 'utf8')
+  })
+}
+
+function sendNotificationIfNeed_turbo() {
+  if (!push_key_turbo) {
     console.log('执行任务结束!'); return;
   }
 
@@ -75,7 +115,7 @@ function sendNotificationIfNeed() {
   let desp = fs.readFileSync(result_path, "utf8")
 
   // 去除末尾的换行
-  let SCKEY = push_key.replace(/[\r\n]/g,"")
+  let SCKEY = push_key_turbo.replace(/[\r\n]/g,"")
 
   const options ={
     uri:  `https://sctapi.ftqq.com/${SCKEY}.send`,
@@ -113,7 +153,10 @@ function main() {
     // 3、执行脚本
     exec(`node '${js_path}' >> '${result_path}'`);
     // 4、发送推送
-    sendNotificationIfNeed() 
+    if(push_key_turbo)
+      sendNotificationIfNeed_turbo()
+    else
+      sendNotificationIfNeed()
   }).catch((err)=>{
     console.log('脚本文件下载失败，任务中断！');
     fs.writeFileSync(error_path, err, 'utf8')
